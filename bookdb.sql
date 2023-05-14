64cf8da021fb9a4ee98b37c3bf9ea3b754ecec97
julilan <juli.molnar@outlook.com>
2023-05-15 00:45:09 +0300

    update SQL Dump

--- bookdb.sql
+++ bookdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: 14.05.2023 klo 20:31
+-- Generation Time: 14.05.2023 klo 21:43
 -- Palvelimen versio: 8.0.32
 -- PHP Version: 8.1.15
 
@@ -42,10 +42,8 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `title`, `author`, `genre`, `description`, `link`, `image`) VALUES
-(1, 'All Systems Red', 'Martha Wells', 'Scifi', 'My favourite book about an antisocial android that loves soap dramas. It&#039;s the best book ever!', 'https://www.goodreads.com/book/show/32758901-all-systems-red', 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1631585309i/32758901.jpg'),
-(9, 'Test 2', 'Test 2', 'Test', 'Hello again', '', ''),
-(10, 'Palaldin', 'Hugo', 'Fantasy', 'asdasd', NULL, NULL),
-(11, 'Hello', 'World', 'Hello', 'Hello', NULL, NULL);
+(1, 'All Systems Red', 'Martha Wells', 'Scifi', 'My favourite book about an antisocial android who loves to watch media and hates dealing with humans.', 'https://www.goodreads.com/book/show/32758901-all-systems-red', 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1631585309i/32758901.jpg'),
+(13, 'The Way of Kings', 'Brandon Sanderson', 'High Fantasy', 'Start of an epic fantasy saga with stellar world-building and characters.', 'https://www.goodreads.com/book/show/7235533-the-way-of-kings?ref=nav_sb_ss_2_9', 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1659905828i/7235533.jpg');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +63,7 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
