7015e82d973af31b3edf77d914ba9d64dd111895
julilan <juli.molnar@outlook.com>
2023-05-14 23:30:48 +0300

    update view action to display selected book data

--- view.php
+++ view.php
@@ -1,5 +1,29 @@
 <?php include 'db.php';
 
+// Validating the id from GET request
+function checkId($id, $mysqli): bool
+{
+    $sql = "SELECT title from books WHERE id = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->num_rows > 0;
+}
+
+if (isset($_GET['view_book'])) {
+    $id = $_GET['view_book'];
+    $idCheck = checkId($id, $mysqli);
+
+    if ($idCheck === false) {
+        // If id is not valid, go back to index.php
+        header('location: index.php');
+    } else {
+        $result = mysqli_query($mysqli, "SELECT * FROM books WHERE id = $id");
+        $book = mysqli_fetch_assoc($result);
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +38,12 @@
 
 <body>
     <div class="book">
-        <p>Title: </p>
-        <p>Author: </p>
-        <p>Genre: </p>
-        <p>Why this book: </p>
-        <p>Link: <a href="#">Goodreads</a> </p>
-        <img src="" alt="">
+        <p>Title: <?= $book['title'] ?></p>
+        <p>Author: <?= $book['author'] ?></p>
+        <p>Genre: <?= $book['genre'] ?></p>
+        <p>Description: <?= $book['description'] ?></p>
+        <p>Link: <a href="<?= $book['link'] ?>">Goodreads</a></p>
+        <img src="<?= $book['image'] ?>" alt="<?= $book['title'] ?>">
     </div>
     <a href="index.php">Back to books</a>
 </body>
