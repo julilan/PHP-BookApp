59b1fb0d9e72289f758bdb869a2d5d9f40ebfdc6
julilan <juli.molnar@outlook.com>
2023-05-17 15:49:13 +0300

    add more styling to index page

--- index.php
+++ index.php
@@ -89,66 +89,81 @@ $books = mysqli_query($mysqli, "SELECT id, title, author, genre FROM books");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="style.css">
     <title>Book App</title>
 </head>
 
 <body>
     <header>
-        <h2>Book recommendations app</h2>
+        <h1>Book App - Recommend your favourite books 📚</h1>
     </header>
-    <form action="index.php" method="POST">
-        <div class="form_group">
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title">
+    <main>
+        <div class="content">
+            <h2 id="form_heading">Add new book recommendation</h2>
+            <form action="index.php" method="POST">
+                <div class="form_group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title">
+                </div>
+                <div class="form_group">
+                    <label for="author">Author</label>
+                    <input type="text" name="author" id="author">
+                </div>
+                <div class="form_group">
+                    <label for="genre">Genre</label>
+                    <input type="text" name="genre" id="genre">
+                </div>
+                <div class="form_group">
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description" cols="30" rows="6"></textarea>
+                </div>
+                <input type="submit" name="submit" value="Add book">
+                <?php if (isset($success_message)) : ?>
+                    <p class="success-message"><?php echo $success_message ?></p>
+                <?php endif; ?>
+                <?php if (isset($insert_error)) : ?>
+                    <p class="error-message"><?php echo $insert_error ?></p>
+                <?php endif; ?>
+                <p class="error-message"><?php if (!empty($errors)) echo $error_message; ?></p>
+            </form>
         </div>
-        <div class="form_group">
-            <label for="author">Author</label>
-            <input type="text" name="author" id="author">
-        </div>
-        <div class="form_group">
-            <label for="genre">Genre</label>
-            <input type="text" name="genre" id="genre">
-        </div>
-        <div class="form_group">
-            <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="6"></textarea>
-        </div>
-        <input type="submit" name="submit" value="Add book">
-        <?php if (isset($success_message)) : ?>
-            <p class="success-message"><?php echo $success_message ?></p>
-        <?php endif; ?>
-        <?php if (isset($insert_error)) : ?>
-            <p class="error-message"><?php echo $insert_error ?></p>
-        <?php endif; ?>
-        <p class="error-message"><?php if (!empty($errors)) echo $error_message; ?></p>
-    </form>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Genre</th>
-                <th>Actions</th>
-            </tr>
+        <div class="content">
+            <h2>My book recommendations</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Genre</th>
+                        <th>Actions</th>
+                    </tr>
 
-        <tbody>
-            <?php while ($row = mysqli_fetch_array($books)) { ?>
-                <tr>
-                    <td><?php echo $row['title']; ?></td>
-                    <td><?php echo $row['author']; ?></td>
-                    <td><?php echo $row['genre']; ?></td>
-                    <td>
-                        <a href="view.php?view_book=<?php echo $row['id']; ?>">view</a>
-                        <a href="update.php?update_book=<?php echo $row['id']; ?>">edit</a>
-                        <a href="index.php?del_book=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this book?')">remove</a>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-        </thead>
-    </table>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_array($books)) { ?>
+                        <tr>
+                            <td><?php echo $row['title']; ?></td>
+                            <td><?php echo $row['author']; ?></td>
+                            <td><?php echo $row['genre']; ?></td>
+                            <td>
+                                <a href="view.php?view_book=<?php echo $row['id']; ?>">
+                                    <span class="material-symbols-outlined">visibility</span>
+                                </a>
+                                <a href="update.php?update_book=<?php echo $row['id']; ?>">
+                                    <span class="material-symbols-outlined">edit</span>
+                                </a>
+                                <a href="index.php?del_book=<?php echo $row['id']; ?>" onclick="return confirm('Are you sure you want to delete this book?')">
+                                    <span class="material-symbols-outlined">delete</span>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+                </thead>
+            </table>
+        </div>
+    </main>
 </body>
 
 </html>
