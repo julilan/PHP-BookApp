42b8540bad1e88dee4313997211e224038a8adce
julilan <juli.molnar@outlook.com>
2023-05-17 16:14:57 +0300

    add favicon

--- view.php
+++ view.php
@@ -33,6 +33,7 @@ if (isset($_GET['view_book'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpg" href="./assets/favicon.ico" />
     <link rel="stylesheet" href="style.css">
     <title>View Book</title>
 </head>
